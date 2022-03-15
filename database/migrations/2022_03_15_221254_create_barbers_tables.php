775356c85feec6f84b1b8992cd1ae7c6a249d9d2
Bruno Alves <vespidhook@gmail.com>
2022-03-15 19:50:22 -0300

    migrations

--- database/migrations/2022_03_15_221254_create_barbers_tables.php
+++ database/migrations/2022_03_15_221254_create_barbers_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBarbersTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('barbers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('avatar')->default('default.png');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->float('stars')->default(0);
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('barbers');
     }
 }
--- database/migrations/2022_03_15_224412_create_barber_reviews_tables.php
+++ database/migrations/2022_03_15_224412_create_barber_reviews_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBarberReviewsTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('barber_reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('avatar')->default('default.png');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('id_barber');
+            $table->float('rate');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('barber_reviews');
     }
 }
