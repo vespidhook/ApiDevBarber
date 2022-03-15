e4a7fafe2bd406922ef26828fa48f99bb5d8b82a
Bruno Alves <vespidhook@gmail.com>
2022-03-15 20:50:30 -0300

    models controllers routes

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,24 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $hidden = ['password'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function getJwtIdentifier()
+    {
+        return $this->getKey();
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Controller\UserController;
+use App\Controller\AuthController;
+use App\Controller\BarberController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function() {
     return ['pong'=>true];
 });
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+Route::post('/user', [AuthController::class, 'create']);
+
+Route::get('/user', [UserController::class, 'read']);
+Route::put('/user', [UserController::class, 'update']);
+Route::get('/user/favorites', [UserController::class, 'getFavorites']);
+Route::post('user/favorite', [UserController::class, 'addFavorite']);
+Route::get('user/appointments' , [UserController::class, 'getAppointments']);
+
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barber/{id}', [BarberController::class, 'one']);
+Route::post('/barber/{id}/appointment', [BarberController::class, 'setAppointment']);
+
+Route::get('/search', [BarberController::class, 'search']);
