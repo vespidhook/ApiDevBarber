7d86e7b5b548c54af1346362c5120c9ea67eecfd
Bruno Alves <vespidhook@gmail.com>
2022-03-20 10:13:35 -0300

    barbeiro 2

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -80,11 +80,60 @@ class BarberController extends Controller
     //     return $array;
     // }
 
+    private function searchGeo($address)
+    {
+        $key = env('MAPS_KEY', null);
+
+        $addresses = urldecode($address);
+        $url = 'https://maps.googleapis.com/maps/api/geocode/json?address='.($address).'&key='.$key;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($res, true);
+    }
+
     public function list(Request $request)
     {
         $array = ['error' => ''];
 
-        $barbers = Barber::all();
+        $lat = $request->input('lat');
+        $lng = $request->input('lng');
+        $city = $request->input('city');
+        $offset = $request->input('offset');
+        if(!$offset) {
+            $offset = 0;
+        }
+
+        if(!empty($city)) {
+            $res = $this->searchGeo($city);
+
+            if(count($res) > 0) {
+                $lat = $res['results'][0]['geometry']['location']['lat'];
+                $lng = $res['results'][0]['geometry']['location']['lng'];
+            }
+        } elseif(!empty($lat) && !empty($lng)) {
+            $res = $this->searchGeo($lat.','.$lng);
+
+            if(count($res['results']) > 0) {
+                $city = $res['results'][0]['formatted_address'];
+            }
+        } else {
+            $lat = '-23.5630907';
+            $lng = '-46.6682795';
+            $cuty = 'São Paulo';
+        }
+
+        $barbers = Barber::select(Barber::raw('*, SQRT(
+            POW(69.1 * (latitude - '.$lat.'), 2) +
+            POW(69.1 * ('.$lng.' - longitude) * COS(latitude / 57.3), 2)) AS distance'))
+            ->havingRaw('distance < ?', [10])
+            ->orderBy('distance', 'ASC')
+            ->offset($offset)
+            ->limit(5)
+            ->get();
 
         foreach ($barbers as $bkey => $bvalue) {
             $barbers[$bkey]['avatar'] = url('media/avatars/'.$barbers[$bkey]['avatar']);
@@ -92,6 +141,42 @@ class BarberController extends Controller
 
         $array['data'] = $barbers;
         $array['loc'] = 'São Paulo';
+
+        return $array;
+    }
+
+    public function one($id)
+    {
+        $array = ['error' => ''];
+
+        $barber = Barber::find($id);
+
+        if($barber) {
+            $barber->avatar = url('media/avatars/'.$barber['avatar']);
+            $barber['favorited'] = false;
+            $barber['photos'] = [];
+            $barber['services'] = [];
+            $barber['testimonials'] = [];
+            $barber['available'] = [];
+
+            // Pegando as fotos do barbeiro
+            $barber['photos'] = BarberPhotos::select(['id', 'url'])->where('id_barber', $barber->id)->get();
+            foreach($barber['photos'] as $bpkey => $bpvalue) {
+                $barber['photos'][$bpkey]['url'] = url('media/uploads/'.$barber['photos'][$bpkey]['url']);
+            }
+
+            // Pegando os serviços do barbeiro
+            $barber['services'] = BarberServices::select(['id', 'name', 'price'])->where('id_barber', $barber->id)->get();
+
+            // Pegando os depoimentos do barbeiro
+            $barber['testimonials'] = BarberTestimonial::select(['id', 'name', 'rate', 'body'])->where('id_barber', $barber->id)->get();
+
+
+            $array['data'] = $barber;
+        } else {
+            $arrey['error'] = 'Barbeiro não existe';
+            return $array;
+        }
 
         return $array;
     }
