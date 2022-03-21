faf439e2b379ea3f6015599365e8262bd2e2643c
Bruno Alves <vespidhook@gmail.com>
2022-03-21 08:09:00 -0300

    disponibilidade

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
+use App\Models\UserAppointment;
 use App\Models\Barber;
 use App\Models\BarberPhotos;
 use App\Models\BarberServices;
@@ -22,63 +23,63 @@ class BarberController extends Controller
         $this->loggedUser = auth()->user();
     }
 
-    // public function random()
-    // {
-    //     $array = ['error'=>''];
+    public function random()
+    {
+        $array = ['error'=>''];
 
-    //     for($q=0; $q<15; $q++) {
-    //         $names = ['Boniek', 'Paulo', 'Pedro', 'Amanda', 'Leticia', 'Gabriel', 'Gabriela', 'Thais', 'Luiz', 'Diogo', 'José', 'Jeremias', 'Francisco', 'Dirce', 'Marcelo' ];
-    //         $lastnames = ['Santos', 'Silva', 'Santos', 'Silva', 'Alvaro', 'Sousa', 'Diniz', 'Josefa', 'Luiz', 'Diogo', 'Limoeiro', 'Santos', 'Limiro', 'Nazare', 'Mimoza' ];
+        for($q=0; $q<15; $q++) {
+            $names = ['Boniek', 'Paulo', 'Pedro', 'Amanda', 'Leticia', 'Gabriel', 'Gabriela', 'Thais', 'Luiz', 'Diogo', 'José', 'Jeremias', 'Francisco', 'Dirce', 'Marcelo' ];
+            $lastnames = ['Santos', 'Silva', 'Santos', 'Silva', 'Alvaro', 'Sousa', 'Diniz', 'Josefa', 'Luiz', 'Diogo', 'Limoeiro', 'Santos', 'Limiro', 'Nazare', 'Mimoza' ];
 
-    //         $servicos = ['Corte', 'Pintura', 'Aparação', 'Unha', 'Progressiva', 'Limpeza de Pele', 'Corte Feminino'];
-    //         $servicos2 = ['Cabelo', 'Unha', 'Pernas', 'Pernas', 'Progressiva', 'Limpeza de Pele', 'Corte Feminino'];
+            $servicos = ['Corte', 'Pintura', 'Aparação', 'Unha', 'Progressiva', 'Limpeza de Pele', 'Corte Feminino'];
+            $servicos2 = ['Cabelo', 'Unha', 'Pernas', 'Pernas', 'Progressiva', 'Limpeza de Pele', 'Corte Feminino'];
 
-    //         $depos = [
-    //             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
-    //             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
-    //             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
-    //             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
-    //             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.'
-    //         ];
+            $depos = [
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.',
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequatur tenetur facere voluptatibus iusto accusantium vero sunt, itaque nisi esse ad temporibus a rerum aperiam cum quaerat quae quasi unde.'
+            ];
 
-    //         $newBarber = new Barber();
-    //         $newBarber->name = $names[rand(0, count($names)-1)].' '.$lastnames[rand(0, count($lastnames)-1)];
-    //         $newBarber->avatar = rand(1, 4).'.png';
-    //         $newBarber->stars = rand(2, 4).'.'.rand(0, 9);
-    //         $newBarber->latitude = '-23.5'.rand(0, 9).'30907';
-    //         $newBarber->longitude = '-46.6'.rand(0,9).'82759';
-    //         $newBarber->save();
+            $newBarber = new Barber();
+            $newBarber->name = $names[rand(0, count($names)-1)].' '.$lastnames[rand(0, count($lastnames)-1)];
+            $newBarber->avatar = rand(1, 4).'.png';
+            $newBarber->stars = rand(2, 4).'.'.rand(0, 9);
+            $newBarber->latitude = '-23.5'.rand(0, 9).'30907';
+            $newBarber->longitude = '-46.6'.rand(0,9).'82759';
+            $newBarber->save();
 
-    //         $ns = rand(3, 6);
+            $ns = rand(3, 6);
 
-    //         for($w=0;$w<4;$w++) {
-    //             $newBarberPhoto = new BarberPhotos();
-    //             $newBarberPhoto->id_barber = $newBarber->id;
-    //             $newBarberPhoto->url = rand(1, 5).'.png';
-    //             $newBarberPhoto->save();
-    //         }
+            for($w=0;$w<4;$w++) {
+                $newBarberPhoto = new BarberPhotos();
+                $newBarberPhoto->id_barber = $newBarber->id;
+                $newBarberPhoto->url = rand(1, 5).'.png';
+                $newBarberPhoto->save();
+            }
 
-    //         for($w=0;$w<$ns;$w++) {
-    //             $newBarberService = new BarberServices();
-    //             $newBarberService->id_barber = $newBarber->id;
-    //             $newBarberService->name = $servicos[rand(0, count($servicos)-1)].' de '.$servicos2[rand(0, count($servicos2)-1)];
-    //             $newBarberService->price = rand(1, 99).'.'.rand(0, 100);
-    //             $newBarberService->save();
-    //         }
+            for($w=0;$w<$ns;$w++) {
+                $newBarberService = new BarberServices();
+                $newBarberService->id_barber = $newBarber->id;
+                $newBarberService->name = $servicos[rand(0, count($servicos)-1)].' de '.$servicos2[rand(0, count($servicos2)-1)];
+                $newBarberService->price = rand(1, 99).'.'.rand(0, 100);
+                $newBarberService->save();
+            }
 
-    //         for($w=0;$w<3;$w++) {
-    //             $newBarberTestimonial = new BarberTestimonial();
-    //             $newBarberTestimonial->id_barber = $newBarber->id;
-    //             $newBarberTestimonial->name = $names[rand(0, count($names)-1)];
-    //             $newBarberTestimonial->rate = rand(2, 4).'.'.rand(0, 9);
-    //             $newBarberTestimonial->body = $depos[rand(0, count($depos)-1)];
-    //             $newBarberTestimonial->save();
-    //         }
+            for($w=0;$w<3;$w++) {
+                $newBarberTestimonial = new BarberTestimonial();
+                $newBarberTestimonial->id_barber = $newBarber->id;
+                $newBarberTestimonial->name = $names[rand(0, count($names)-1)];
+                $newBarberTestimonial->rate = rand(2, 4).'.'.rand(0, 9);
+                $newBarberTestimonial->body = $depos[rand(0, count($depos)-1)];
+                $newBarberTestimonial->save();
+            }
 
 
-    //         }
-    //     return $array;
-    // }
+            }
+        return $array;
+    }
 
     private function searchGeo($address)
     {
@@ -171,6 +172,55 @@ class BarberController extends Controller
             // Pegando os depoimentos do barbeiro
             $barber['testimonials'] = BarberTestimonial::select(['id', 'name', 'rate', 'body'])->where('id_barber', $barber->id)->get();
 
+            // Pegando disponibilidade do barbeiro
+            $availability = [];
+
+            // - Pegando a disponibilidade crua
+            $avails = BarberAvailability::where('id_barber', $barber->id)->get();
+            $availsWeekdays = [];
+            foreach ($avails as $item) {
+                $availsWeekdays[$item['weekday']] = explode(',', $item['hours']);
+            }
+
+            // - Pegar os agendamentos dos próximos 20 dias
+            $appointments = [];
+            $appQuery = UserAppointment::where('id_barber', $barber->id)
+                ->whereBetween('ap_datetime', [
+                    date('Y-m-d').' 00:00:00',
+                    date('Y-m-d', strtotime('+20 days')).' 23:59:59'
+                ])
+                ->get();
+            foreach ($appQuery as $appItem) {
+                $appointments[] = $appItem['ap_datetime'];
+            }
+
+            // - Gerar disponibilidade real
+            for($q=0;$q<20;$q++) {
+                $timeItem = strtoitem('+'.$q.' days');
+                $weekday = date('w', $timeItem);
+
+                if(in_array($weekday, array_keys($availsWeekdays))) {
+                    $hours = [];
+
+                    $dayItem = date('Y-m-d', $timeItem);
+
+                    foreach($availsWeekdays[$weekday] as $hourItem) {
+                        $dayFormated = $dayItem.' '.$hourItem.':00';
+                        if(in_array($dayFormated, $appointments)) {
+                            $hours[] = $hourItem;
+                        }
+                    }
+
+                    if(count($hours) > 0) {
+                        $availability[] = [
+                            'date' => $dayItem,
+                            'hours' => $hours
+                        ];
+                    }
+                }
+            }
+
+            $barber['available'] = $availability;
 
             $array['data'] = $barber;
         } else {
